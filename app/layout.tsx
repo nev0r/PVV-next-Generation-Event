cc37146d9cfa199e60916317e91126d2f2c021a0
Kamp <108412816+nev0r@users.noreply.github.com>
2026-03-06 12:01:52 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "PVV Next Generation Event",
+  description: "PVV AG Kundenveranstaltung",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
